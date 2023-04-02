c4221359b2f507d2b7419578c145245d043f5c6a
Rich <richard.g.otap@gmail.com>
2023-04-02 10:24:28 -0700

    flake: Run formatter.

--- flake.nix
+++ flake.nix
@@ -8,69 +8,78 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      # Overlays enable you to customize the Nixpkgs attribute set
-      overlays = [
-        # Makes a `rust-bin` attribute available in Nixpkgs
-        (import rust-overlay)
-        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
-        # create a Rust environment
-        (self: super: {
-          rustToolchain = super.rust-bin.stable.latest.default;
-        })
-      ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    # Overlays enable you to customize the Nixpkgs attribute set
+    overlays = [
+      # Makes a `rust-bin` attribute available in Nixpkgs
+      (import rust-overlay)
+      # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+      # create a Rust environment
+      (self: super: {
+        rustToolchain = super.rust-bin.stable.latest.default;
+      })
+    ];
 
-      # Systems supported
-      allSystems = [
-        "x86_64-linux" # 64-bit Intel/AMD Linux
-        "aarch64-linux" # 64-bit ARM Linux
-        "x86_64-darwin" # 64-bit Intel macOS
-        "aarch64-darwin" # 64-bit ARM macOS
-      ];
+    # Systems supported
+    allSystems = [
+      "x86_64-linux" # 64-bit Intel/AMD Linux
+      "aarch64-linux" # 64-bit ARM Linux
+      "x86_64-darwin" # 64-bit Intel macOS
+      "aarch64-darwin" # 64-bit ARM macOS
+    ];
 
-      # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      packages = forAllSystems ({ pkgs }: {
-        default = pkgs.rustPlatform.buildRustPackage {
-          name = "stable-diffusion-bot";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-          buildInputs = [
+    # Helper to provide system-specific attributes
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    packages = forAllSystems ({pkgs}: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        name = "stable-diffusion-bot";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        nativeBuildInputs = [
+          pkgs.pkg-config
+        ];
+        buildInputs =
+          [
             pkgs.openssl
             pkgs.sqlite
-          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (
-            let frameworks = pkgs.darwin.apple_sdk.frameworks;
-            in
-            [
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (
+            let
+              frameworks = pkgs.darwin.apple_sdk.frameworks;
+            in [
               frameworks.Security
               frameworks.CoreFoundation
               frameworks.CoreServices
-            ]);
+            ]
+          );
 
-          doCheck = false;
-        };
-      });
-      # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default = pkgs.mkShell {
-          buildInputs = [ pkgs.pkg-config pkgs.openssl.dev pkgs.sqlite ];
-          # The Nix packages provided in the environment
-          packages = (with pkgs; [
+        doCheck = false;
+      };
+    });
+    # Development environment output
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        buildInputs = [pkgs.pkg-config pkgs.openssl.dev pkgs.sqlite];
+        # The Nix packages provided in the environment
+        packages =
+          (with pkgs; [
             # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
             # rustdoc, rustfmt, and other tools.
             rustToolchain
-          ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
-        };
-      });
-    };
+          ])
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [libiconv]);
+      };
+    });
+  };
 }
