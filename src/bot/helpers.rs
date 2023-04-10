9db90dbaaddb635a71faf693c6ef4f33ed532264
Richard Otap <richard.g.otap@gmail.com>
2023-04-09 19:45:20 -0700

    bot: Document `get_file()` function.

--- src/bot/helpers.rs
+++ src/bot/helpers.rs
@@ -2,6 +2,27 @@ use anyhow::Context;
 use futures::TryStreamExt;
 use teloxide::{net::Download, types::File, Bot};
 
+/// Download a Telegram `File` and return its contents as bytes.
+///
+/// # Examples
+///
+/// ```no_run
+/// use telegram_bot::*;
+///
+/// async fn handle_photo(bot: Bot, message: Message) -> Result<(), Box<dyn std::error::Error>> {
+///     if let Some(photos) = message.photo(){
+///         if let Some(photo) = photos.last() {
+///             let file = bot.get_file(&photo.file.id).await?;
+///             let bytes = get_file(&bot, &file)?;
+///
+///             // ... do something with the photo bytes ...
+///
+///         }
+///     }
+///
+///     Ok(())
+/// }
+/// ```
 pub(crate) async fn get_file(bot: &Bot, file: &File) -> anyhow::Result<bytes::Bytes> {
     bot.download_file_stream(&file.path)
         .try_collect()
