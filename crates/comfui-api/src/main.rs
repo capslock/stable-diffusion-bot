d2e4c4773dd552c6542c30dd79eaf8a4613cf2d1
Rich <richard.g.otap@gmail.com>
2023-11-27 14:15:09 -0800

    comfyui: Basic ComfyUI support.

---
+++ crates/comfui-api/src/main.rs
@@ -0,0 +1,97 @@
+use comfyui_api::Prompt;
+
+fn main() {
+    let prompt: Prompt = serde_json::from_str(
+        r#"
+    {
+        "3": {
+            "class_type": "KSampler",
+            "inputs": {
+                "cfg": 8,
+                "denoise": 1,
+                "latent_image": [
+                    "5",
+                    0
+                ],
+                "model": [
+                    "4",
+                    0
+                ],
+                "negative": [
+                    "7",
+                    0
+                ],
+                "positive": [
+                    "6",
+                    0
+                ],
+                "sampler_name": "euler",
+                "scheduler": "normal",
+                "seed": 8566257,
+                "steps": 20
+            }
+        },
+        "4": {
+            "class_type": "CheckpointLoaderSimple",
+            "inputs": {
+                "ckpt_name": "v1-5-pruned-emaonly.ckpt"
+            }
+        },
+        "5": {
+            "class_type": "EmptyLatentImage",
+            "inputs": {
+                "batch_size": 1,
+                "height": 512,
+                "width": 512
+            }
+        },
+        "6": {
+            "class_type": "CLIPTextEncode",
+            "inputs": {
+                "clip": [
+                    "4",
+                    1
+                ],
+                "text": "masterpiece best quality girl"
+            }
+        },
+        "7": {
+            "class_type": "CLIPTextEncode",
+            "inputs": {
+                "clip": [
+                    "4",
+                    1
+                ],
+                "text": "bad hands"
+            }
+        },
+        "8": {
+            "class_type": "VAEDecode",
+            "inputs": {
+                "samples": [
+                    "3",
+                    0
+                ],
+                "vae": [
+                    "4",
+                    2
+                ]
+            }
+        },
+        "9": {
+            "class_type": "SaveImage",
+            "inputs": {
+                "filename_prefix": "ComfyUI",
+                "images": [
+                    "8",
+                    0
+                ]
+            }
+        }
+    }
+    "#,
+    )
+    .unwrap();
+    println!("{:#?}", prompt);
+    println!("{}", serde_json::to_string_pretty(&prompt).unwrap());
+}
