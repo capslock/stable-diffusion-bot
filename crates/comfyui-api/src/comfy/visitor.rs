c5cb2e5d33ddeb17957d2078c30f97d9ab86a438
Rich <richard.g.otap@gmail.com>
2023-12-02 15:42:20 -0800

    comfyui: Add documentation.

--- crates/comfyui-api/src/comfy/visitor.rs
+++ crates/comfyui-api/src/comfy/visitor.rs
@@ -1,75 +1,215 @@
 use crate::models::*;
 
+/// Trait for visiting nodes in a ComfyUI graph.
 pub trait Visitor {
+    /// Returns a reference to the node with the given id.
     fn get_node_by_id(&self, id: &str) -> &NodeOrUnknown;
+
+    /// Visits a node or unknown node. This is the entry point for the visitor.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The node to visit.
     fn visit_node_or_unknown(&mut self, node: &NodeOrUnknown) {
         walk_node_or_unknown(self, node);
     }
+
+    /// Visits a node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The node to visit.
     fn visit_node(&mut self, node: &Node) {
         walk_node(self, node);
     }
+
+    /// Visits a generic node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The generic node to visit.
     fn visit_generic_node(&mut self, node: &GenericNode) {
         walk_generic_node(self, node);
     }
+
+    /// Visits a KSampler node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The KSampler node to visit.
     fn visit_k_sampler(&mut self, node: &KSampler) {
         walk_k_sampler(self, node);
     }
+
+    /// Visits a CLIPTextEncode node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The CLIPTextEncode node to visit.
     fn visit_clip_text_encode(&mut self, node: &CLIPTextEncode) {
         walk_clip_text_encode(self, node);
     }
+
+    /// Visits an EmptyLatentImage node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The EmptyLatentImage node to visit.
     fn visit_empty_latent_image(&mut self, node: &EmptyLatentImage) {
         walk_empty_latent_image(self, node);
     }
+
+    /// Visits a CheckpointLoaderSimple node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The CheckpointLoaderSimple node to visit.
     fn visit_checkpoint_loader_simple(&mut self, node: &CheckpointLoaderSimple) {
         walk_checkpoint_loader_simple(self, node);
     }
+
+    /// Visits a VAELoader node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The VAELoader node to visit.
     fn visit_vae_loader(&mut self, node: &VAELoader) {
         walk_vae_loader(self, node);
     }
+
+    /// Visits a VAEDecode node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The VAEDecode node to visit.
     fn visit_vae_decode(&mut self, node: &VAEDecode) {
         walk_vae_decode(self, node);
     }
+
+    /// Visits a PreviewImage node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The PreviewImage node to visit.
     fn visit_preview_image(&mut self, node: &PreviewImage) {
         walk_preview_image(self, node);
     }
+
+    /// Visits a KSamplerSelect node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The KSamplerSelect node to visit.
     fn visit_k_sampler_select(&mut self, node: &KSamplerSelect) {
         walk_k_sampler_select(self, node);
     }
+
+    /// Visits a SamplerCustom node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The SamplerCustom node to visit.
     fn visit_sampler_custom(&mut self, node: &SamplerCustom) {
         walk_sampler_custom(self, node);
     }
+
+    /// Visits a SDTurboScheduler node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The SDTurboScheduler node to visit.
     fn visit_sd_turbo_scheduler(&mut self, node: &SDTurboScheduler) {
         walk_sd_turbo_scheduler(self, node);
     }
+
+    /// Visits a ImageOnlyCheckpointLoader node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The ImageOnlyCheckpointLoader node to visit.
     fn visit_image_only_checkpoint_loader(&mut self, node: &ImageOnlyCheckpointLoader) {
         walk_image_only_checkpoint_loader(self, node);
     }
+
+    /// Visits a LoadImage node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The LoadImage node to visit.
     fn visit_load_image(&mut self, node: &LoadImage) {
         walk_load_image(self, node);
     }
+
+    /// Visits a SVDimg2vidConditioning node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The SVDimg2vidConditioning node to visit.
     fn visit_svdimg2vid_conditioning(&mut self, node: &SVDimg2vidConditioning) {
         walk_svdimg2vid_conditioning(self, node);
     }
+
+    /// Visits a VideoLinearCFGGuidance node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The VideoLinearCFGGuidance node to visit.
     fn visit_video_linear_cfg_guidance(&mut self, node: &VideoLinearCFGGuidance) {
         walk_video_linear_cfg_guidance(self, node);
     }
+
+    /// Visits a SaveAnimatedWEBP node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The SaveAnimatedWEBP node to visit.
     fn visit_save_animated_webp(&mut self, node: &SaveAnimatedWEBP) {
         walk_save_animated_webp(self, node);
     }
+
+    /// Visits a LoraLoader node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The LoraLoader node to visit.
     fn visit_lora_loader(&mut self, node: &LoraLoader) {
         walk_lora_loader(self, node);
     }
+
+    /// Visits a ModelSamplingDiscrete node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The ModelSamplingDiscrete node to visit.
     fn visit_model_sampling_discrete(&mut self, node: &ModelSamplingDiscrete) {
         walk_model_sampling_discrete(self, node);
     }
+
+    /// Visits a SaveImage node.
+    ///
+    /// # Arguments
+    ///
+    /// * `node` - The SaveImage node to visit.
     fn visit_save_image(&mut self, node: &SaveImage) {
         walk_save_image(self, node);
     }
+
+    /// Visits an input.
+    ///
+    /// # Arguments
+    ///
+    /// * `input` - The input to visit.
     fn visit_input<T>(&mut self, input: &Input<T>) {
         walk_input(self, input);
     }
 }
 
+/// Traverse a node or unknown node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `node` - The node to traverse.
 pub fn walk_node_or_unknown<V: Visitor + ?Sized>(visitor: &mut V, node: &NodeOrUnknown) {
     match node {
         NodeOrUnknown::Node(node) => visitor.visit_node(node),
@@ -77,6 +217,12 @@ pub fn walk_node_or_unknown<V: Visitor + ?Sized>(visitor: &mut V, node: &NodeOrU
     }
 }
 
+/// Traverse a node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `node` - The node to traverse.
 pub fn walk_node<V: Visitor + ?Sized>(visitor: &mut V, node: &Node) {
     match *node {
         Node::KSampler(ref node) => visitor.visit_k_sampler(node),
@@ -102,6 +248,12 @@ pub fn walk_node<V: Visitor + ?Sized>(visitor: &mut V, node: &Node) {
     }
 }
 
+/// Traverse a generic node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `node` - The generic node to traverse.
 pub fn walk_generic_node<V: Visitor + ?Sized>(visitor: &mut V, n: &GenericNode) {
     for (_, value) in n.inputs.iter() {
         if let GenericValue::NodeConnection(node_connection) = value {
@@ -111,12 +263,24 @@ pub fn walk_generic_node<V: Visitor + ?Sized>(visitor: &mut V, n: &GenericNode) 
     }
 }
 
+/// Traverse an input.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `input` - The input to traverse.
 pub fn walk_input<V: Visitor + ?Sized, T>(visitor: &mut V, input: &Input<T>) {
     if let Input::NodeConnection(node_connection) = input {
         visitor.visit_node_or_unknown(&visitor.get_node_by_id(&node_connection.node_id).clone());
     }
 }
 
+/// Traverse a KSampler node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The KSampler node to traverse.
 pub fn walk_k_sampler<V: Visitor + ?Sized>(visitor: &mut V, n: &KSampler) {
     visitor.visit_input(&n.cfg);
     visitor.visit_input(&n.denoise);
@@ -130,16 +294,34 @@ pub fn walk_k_sampler<V: Visitor + ?Sized>(visitor: &mut V, n: &KSampler) {
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.latent_image.node_id).clone());
 }
 
+/// Traverse a CLIPTextEncode node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The CLIPTextEncode node to traverse.
 pub fn walk_clip_text_encode<V: Visitor + ?Sized>(visitor: &mut V, n: &CLIPTextEncode) {
     visitor.visit_input(&n.text);
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.clip.node_id).clone());
 }
 
+/// Traverse an EmptyLatentImage node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The EmptyLatentImage node to traverse.
 pub fn walk_empty_latent_image<V: Visitor + ?Sized>(visitor: &mut V, n: &EmptyLatentImage) {
     visitor.visit_input(&n.width);
     visitor.visit_input(&n.height);
 }
 
+/// Traverse a CheckpointLoaderSimple node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The CheckpointLoaderSimple node to traverse.
 pub fn walk_checkpoint_loader_simple<V: Visitor + ?Sized>(
     visitor: &mut V,
     n: &CheckpointLoaderSimple,
@@ -147,23 +329,53 @@ pub fn walk_checkpoint_loader_simple<V: Visitor + ?Sized>(
     visitor.visit_input(&n.ckpt_name);
 }
 
+/// Traverse a VAELoader node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The VAELoader node to traverse.
 pub fn walk_vae_loader<V: Visitor + ?Sized>(visitor: &mut V, n: &VAELoader) {
     visitor.visit_input(&n.vae_name);
 }
 
+/// Traverse a VAEDecode node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The VAEDecode node to traverse.
 pub fn walk_vae_decode<V: Visitor + ?Sized>(visitor: &mut V, n: &VAEDecode) {
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.vae.node_id).clone());
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.samples.node_id).clone());
 }
 
+/// Traverse a PreviewImage node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The PreviewImage node to traverse.
 pub fn walk_preview_image<V: Visitor + ?Sized>(visitor: &mut V, n: &PreviewImage) {
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.images.node_id).clone());
 }
 
+/// Traverse a KSamplerSelect node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The KSamplerSelect node to traverse.
 pub fn walk_k_sampler_select<V: Visitor + ?Sized>(visitor: &mut V, n: &KSamplerSelect) {
     visitor.visit_input(&n.sampler_name);
 }
 
+/// Traverse a SamplerCustom node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The SamplerCustom node to traverse.
 pub fn walk_sampler_custom<V: Visitor + ?Sized>(visitor: &mut V, n: &SamplerCustom) {
     visitor.visit_input(&n.add_noise);
     visitor.visit_input(&n.cfg);
@@ -176,11 +388,23 @@ pub fn walk_sampler_custom<V: Visitor + ?Sized>(visitor: &mut V, n: &SamplerCust
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.sigmas.node_id).clone());
 }
 
+/// Traverse a SDTurboScheduler node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The SDTurboScheduler node to traverse.
 pub fn walk_sd_turbo_scheduler<V: Visitor + ?Sized>(visitor: &mut V, n: &SDTurboScheduler) {
     visitor.visit_input(&n.steps);
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.model.node_id).clone());
 }
 
+/// Traverse a ImageOnlyCheckpointLoader node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The ImageOnlyCheckpointLoader node to traverse.
 pub fn walk_image_only_checkpoint_loader<V: Visitor + ?Sized>(
     visitor: &mut V,
     n: &ImageOnlyCheckpointLoader,
@@ -188,11 +412,23 @@ pub fn walk_image_only_checkpoint_loader<V: Visitor + ?Sized>(
     visitor.visit_input(&n.ckpt_name);
 }
 
+/// Traverse a LoadImage node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The LoadImage node to traverse.
 pub fn walk_load_image<V: Visitor + ?Sized>(visitor: &mut V, n: &LoadImage) {
     visitor.visit_input(&n.file_to_upload);
     visitor.visit_input(&n.image);
 }
 
+/// Traverse a SVDimg2vidConditioning node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The SVDimg2vidConditioning node to traverse.
 pub fn walk_svdimg2vid_conditioning<V: Visitor + ?Sized>(
     visitor: &mut V,
     n: &SVDimg2vidConditioning,
@@ -208,6 +444,12 @@ pub fn walk_svdimg2vid_conditioning<V: Visitor + ?Sized>(
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.vae.node_id).clone());
 }
 
+/// Traverse a VideoLinearCFGGuidance node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The VideoLinearCFGGuidance node to traverse.
 pub fn walk_video_linear_cfg_guidance<V: Visitor + ?Sized>(
     visitor: &mut V,
     n: &VideoLinearCFGGuidance,
@@ -216,6 +458,12 @@ pub fn walk_video_linear_cfg_guidance<V: Visitor + ?Sized>(
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.model.node_id).clone());
 }
 
+/// Traverse a SaveAnimatedWEBP node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The SaveAnimatedWEBP node to traverse.
 pub fn walk_save_animated_webp<V: Visitor + ?Sized>(visitor: &mut V, n: &SaveAnimatedWEBP) {
     visitor.visit_input(&n.filename_prefix);
     visitor.visit_input(&n.fps);
@@ -225,6 +473,12 @@ pub fn walk_save_animated_webp<V: Visitor + ?Sized>(visitor: &mut V, n: &SaveAni
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.images.node_id).clone());
 }
 
+/// Traverse a LoraLoader node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The LoraLoader node to traverse.
 pub fn walk_lora_loader<V: Visitor + ?Sized>(visitor: &mut V, n: &LoraLoader) {
     visitor.visit_input(&n.lora_name);
     visitor.visit_input(&n.strength_model);
@@ -233,6 +487,12 @@ pub fn walk_lora_loader<V: Visitor + ?Sized>(visitor: &mut V, n: &LoraLoader) {
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.clip.node_id).clone());
 }
 
+/// Traverse a ModelSamplingDiscrete node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The ModelSamplingDiscrete node to traverse.
 pub fn walk_model_sampling_discrete<V: Visitor + ?Sized>(
     visitor: &mut V,
     n: &ModelSamplingDiscrete,
@@ -242,6 +502,12 @@ pub fn walk_model_sampling_discrete<V: Visitor + ?Sized>(
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.model.node_id).clone());
 }
 
+/// Traverse a SaveImage node.
+///
+/// # Arguments
+///
+/// * `visitor` - The visitor to use.
+/// * `n` - The SaveImage node to traverse.
 pub fn walk_save_image<V: Visitor + ?Sized>(visitor: &mut V, n: &SaveImage) {
     visitor.visit_input(&n.filename_prefix);
     visitor.visit_node_or_unknown(&visitor.get_node_by_id(&n.images.node_id).clone());
