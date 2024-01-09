e34661457256e09c7107ad5d3441c47ce97f8d63
Rich <richard.g.otap@gmail.com>
2024-01-08 21:02:11 -0800

    comfyui: Introduce more specific error type for `ViewApi`.

--- crates/comfyui-api/src/api/view.rs
+++ crates/comfyui-api/src/api/view.rs
@@ -1,7 +1,31 @@
-use anyhow::Context;
 use reqwest::Url;
 
 use crate::models::Image;
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum ViewApiError {
+    /// Error parsing endpoint URL
+    #[error("Failed to parse endpoint URL")]
+    ParseError(#[from] url::ParseError),
+    /// Error sending request
+    #[error("Failed to send request")]
+    RequestFailed(#[from] reqwest::Error),
+    /// An error occurred getting response bytes.
+    #[error("Failed to get response bytes")]
+    GetBytesFailed(#[source] reqwest::Error),
+    /// An error occurred getting response text.
+    #[error("Failed to get response text")]
+    GetTextFailed(#[source] reqwest::Error),
+    /// Server returned an error when uploading file
+    #[error("Failed to upload image: {status}: {error}")]
+    ViewImageFailed {
+        status: reqwest::StatusCode,
+        error: String,
+    },
+}
+
+type Result<T> = std::result::Result<T, ViewApiError>;
 
 /// Struct representing a connection to the ComfyUI API `view` endpoint.
 #[derive(Clone, Debug)]
@@ -22,14 +46,11 @@ impl ViewApi {
     /// # Returns
     ///
     /// A `Result` containing a new `ViewApi` instance on success, or an error if url parsing failed.
-    pub fn new<S>(client: reqwest::Client, endpoint: S) -> anyhow::Result<Self>
+    pub fn new<S>(client: reqwest::Client, endpoint: S) -> Result<Self>
     where
         S: AsRef<str>,
     {
-        Ok(Self::new_with_url(
-            client,
-            Url::parse(endpoint.as_ref()).context("failed to parse endpoint url")?,
-        ))
+        Ok(Self::new_with_url(client, Url::parse(endpoint.as_ref())?))
     }
 
     /// Constructs a new `ViewApi` client with a given `reqwest::Client` and endpoint `Url`.
@@ -56,30 +77,25 @@ impl ViewApi {
     ///
     /// A `Result` containing a `Vec<u8>` representation of the image on
     /// success, or an error if the request failed.
-    pub async fn get(&self, image: &Image) -> anyhow::Result<Vec<u8>> {
+    pub async fn get(&self, image: &Image) -> Result<Vec<u8>> {
         let response = self
             .client
             .get(self.endpoint.clone())
             .query(&image)
             .send()
-            .await
-            .context("failed to send request")?;
+            .await?;
         if response.status().is_success() {
             return Ok(response
                 .bytes()
                 .await
-                .context("failed to get bytes")?
+                .map_err(ViewApiError::GetBytesFailed)?
                 .to_vec());
         }
         let status = response.status();
-        let text = response
-            .text()
-            .await
-            .context("failed to get response text")?;
-        Err(anyhow::anyhow!(
-            "got error code: {}, message text: {}",
+        let text = response.text().await.map_err(ViewApiError::GetTextFailed)?;
+        Err(ViewApiError::ViewImageFailed {
             status,
-            text
-        ))
+            error: text,
+        })
     }
 }
