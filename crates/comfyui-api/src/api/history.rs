87f4ec3b46d62d3d663219f29d711a631c2815e9
Rich <richard.g.otap@gmail.com>
2024-01-08 20:47:04 -0800

    comfyui: Introduce more specific error type for `HistoryApi`.

--- crates/comfyui-api/src/api/history.rs
+++ crates/comfyui-api/src/api/history.rs
@@ -1,7 +1,41 @@
-use anyhow::Context;
 use reqwest::Url;
 
 use crate::models::{History, Task};
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum HistoryApiError {
+    /// Error parsing endpoint URL
+    #[error("Failed to parse endpoint URL")]
+    ParseError(#[from] url::ParseError),
+    /// Error sending request
+    #[error("Failed to send request")]
+    RequestFailed(#[from] reqwest::Error),
+    /// An error occurred while parsing the response from the API.
+    #[error("Parsing response failed")]
+    InvalidResponse(#[source] reqwest::Error),
+    /// An error occurred getting response data.
+    #[error("Failed to get response data")]
+    GetDataFailed(#[source] reqwest::Error),
+    /// Server returned an error getting history
+    #[error("Failed to get history: {status}: {error}")]
+    GetHistoryFailed {
+        status: reqwest::StatusCode,
+        error: String,
+    },
+    /// Task was not found
+    #[error("Task not found: {0}")]
+    TaskNotFound(uuid::Uuid),
+    /// Server returned an error getting task
+    #[error("Failed to get task {task}: {status}: {error}")]
+    GetTaskFailed {
+        task: uuid::Uuid,
+        status: reqwest::StatusCode,
+        error: String,
+    },
+}
+
+type Result<T> = std::result::Result<T, HistoryApiError>;
 
 /// Struct representing a connection to the ComfyUI API `history` endpoint.
 #[derive(Clone, Debug)]
@@ -22,14 +56,11 @@ impl HistoryApi {
     /// # Returns
     ///
     /// A `Result` containing a new `HistoryApi` instance on success, or an error if url parsing failed.
-    pub fn new<S>(client: reqwest::Client, endpoint: S) -> anyhow::Result<Self>
+    pub fn new<S>(client: reqwest::Client, endpoint: S) -> Result<Self>
     where
         S: AsRef<str>,
     {
-        Ok(Self::new_with_url(
-            client,
-            Url::parse(endpoint.as_ref()).context("failed to parse endpoint url")?,
-        ))
+        Ok(Self::new_with_url(client, Url::parse(endpoint.as_ref())?))
     }
 
     /// Constructs a new `HistoryApi` client with a given `reqwest::Client` and endpoint `Url`.
@@ -51,26 +82,23 @@ impl HistoryApi {
     /// # Returns
     ///
     /// A `Result` containing a `History` on success, or an error if the request failed.
-    pub async fn get(&self) -> anyhow::Result<History> {
-        let response = self
-            .client
-            .get(self.endpoint.clone())
-            .send()
-            .await
-            .context("failed to send request")?;
+    pub async fn get(&self) -> Result<History> {
+        let response = self.client.get(self.endpoint.clone()).send().await?;
         if response.status().is_success() {
-            return response.json().await.context("failed to parse json");
+            return response
+                .json()
+                .await
+                .map_err(HistoryApiError::InvalidResponse);
         }
         let status = response.status();
         let text = response
             .text()
             .await
-            .context("failed to get response text")?;
-        Err(anyhow::anyhow!(
-            "got error code: {}, message text: {}",
+            .map_err(HistoryApiError::GetDataFailed)?;
+        Err(HistoryApiError::GetHistoryFailed {
             status,
-            text
-        ))
+            error: text,
+        })
     }
 
     /// Sends a history request using the HistoryApi client.
@@ -82,34 +110,31 @@ impl HistoryApi {
     /// # Returns
     ///
     /// A `Result` containing a `Task` on success, or an error if the request failed.
-    pub async fn get_prompt(&self, prompt_id: &uuid::Uuid) -> anyhow::Result<Task> {
+    pub async fn get_prompt(&self, prompt_id: &uuid::Uuid) -> Result<Task> {
         let response = self
             .client
-            .get(
-                self.endpoint
-                    .clone()
-                    .join(prompt_id.to_string().as_str())
-                    .context("failed to parse url")?,
-            )
+            .get(self.endpoint.clone().join(prompt_id.to_string().as_str())?)
             .send()
-            .await
-            .context("failed to send request")?;
+            .await?;
         if response.status().is_success() {
-            let mut history: History = response.json().await.context("failed to parse json")?;
+            let mut history: History = response
+                .json()
+                .await
+                .map_err(HistoryApiError::InvalidResponse)?;
             return history
                 .tasks
                 .remove(prompt_id)
-                .context("failed to get task");
+                .ok_or_else(|| HistoryApiError::TaskNotFound(*prompt_id));
         }
         let status = response.status();
         let text = response
             .text()
             .await
-            .context("failed to get response text")?;
-        Err(anyhow::anyhow!(
-            "got error code: {}, message text: {}",
+            .map_err(HistoryApiError::GetDataFailed)?;
+        Err(HistoryApiError::GetTaskFailed {
+            task: *prompt_id,
             status,
-            text
-        ))
+            error: text,
+        })
     }
 }
