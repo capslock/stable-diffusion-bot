8291d31d2b6fc3cb80b0e8b3631c3be3bf8e4e1c
Rich <richard.g.otap@gmail.com>
2024-01-08 20:51:28 -0800

    comfyui: Introduce more specific error type for `PromptApi`.

--- crates/comfyui-api/src/api/prompt.rs
+++ crates/comfyui-api/src/api/prompt.rs
@@ -1,9 +1,33 @@
-use anyhow::Context;
 use reqwest::Url;
 use serde::Serialize;
 use serde_with::skip_serializing_none;
 
 use crate::models::{Prompt, Response};
+
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum PromptApiError {
+    /// Error parsing endpoint URL
+    #[error("Failed to parse endpoint URL")]
+    ParseError(#[from] url::ParseError),
+    /// Error sending request
+    #[error("Failed to send request")]
+    RequestFailed(#[from] reqwest::Error),
+    /// An error occurred while parsing the response from the API.
+    #[error("Parsing response failed")]
+    InvalidResponse(#[source] reqwest::Error),
+    /// An error occurred getting response data.
+    #[error("Failed to get response data")]
+    GetDataFailed(#[source] reqwest::Error),
+    /// Server returned an error when sending prompt
+    #[error("Failed to send prompt: {status}: {error}")]
+    SendPromptFailed {
+        status: reqwest::StatusCode,
+        error: String,
+    },
+}
+
+type Result<T> = std::result::Result<T, PromptApiError>;
 
 #[derive(Serialize, Debug)]
 #[skip_serializing_none]
@@ -32,17 +56,13 @@ impl PromptApi {
     /// # Returns
     ///
     /// A `Result` containing a new `PromptApi` instance on success, or an error if url parsing failed.
-    pub fn new<S>(
-        client: reqwest::Client,
-        endpoint: S,
-        client_id: uuid::Uuid,
-    ) -> anyhow::Result<Self>
+    pub fn new<S>(client: reqwest::Client, endpoint: S, client_id: uuid::Uuid) -> Result<Self>
     where
         S: AsRef<str>,
     {
         Ok(Self::new_with_url(
             client,
-            Url::parse(endpoint.as_ref()).context("failed to parse endpoint url")?,
+            Url::parse(endpoint.as_ref())?,
             client_id,
         ))
     }
@@ -74,15 +94,11 @@ impl PromptApi {
     /// # Returns
     ///
     /// A `Result` containing a `Response` on success, or an error if the request failed.
-    pub async fn send(&self, prompt: &Prompt) -> anyhow::Result<Response> {
+    pub async fn send(&self, prompt: &Prompt) -> Result<Response> {
         self.send_as_client(prompt, self.client_id).await
     }
 
-    async fn send_as_client(
-        &self,
-        prompt: &Prompt,
-        client_id: uuid::Uuid,
-    ) -> anyhow::Result<Response> {
+    async fn send_as_client(&self, prompt: &Prompt, client_id: uuid::Uuid) -> Result<Response> {
         let response = self
             .client
             .post(self.endpoint.clone())
@@ -91,21 +107,22 @@ impl PromptApi {
                 client_id: Some(client_id),
             })
             .send()
-            .await
-            .context("failed to send request")?;
+            .await?;
         if response.status().is_success() {
-            return response.json().await.context("failed to parse json");
+            return response
+                .json()
+                .await
+                .map_err(PromptApiError::InvalidResponse);
         }
         let status = response.status();
         let text = response
             .text()
             .await
-            .context("failed to get response text")?;
-        Err(anyhow::anyhow!(
-            "got error code: {}, message text: {}",
+            .map_err(PromptApiError::GetDataFailed)?;
+        Err(PromptApiError::SendPromptFailed {
             status,
-            text
-        ))
+            error: text,
+        })
     }
 
     /// Returns the client id used for requests.
